d66ebbcab6e26e24719a78126ceaf512ebff6a1a
Lea Park <ayuminee2@gmail.com>
2023-10-21 18:32:51 +0900

    Ops: nginx rate limit location

--- server/main.py
+++ server/main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/limits")
+async def get_limits():
+    return {"message": "Hello Limits"}
+
+
+@app.get("/limits/{limit_id}")
+async def get_limits(limit_id: int):
+    return {"message": f"Hello Limits {limit_id}"}
